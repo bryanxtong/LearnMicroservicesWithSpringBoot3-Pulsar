e4ffedb748e6403abd9258841456452c2f472918
bryanxtong <bryanxtong@gmail.com>
2024-09-05 08:38:10 +0800

    remove files

--- gamification/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-import org.apache.pulsar.client.impl.ProducerImpl;
-
-public class Test {
-    public static void main(String[] args) {
-       final String PULSAR_LOGGER_PREFIX = ProducerImpl.class.getPackage().getName().replaceFirst("\\.impl$", "");
-       System.out.println(PULSAR_LOGGER_PREFIX);
-
-    }
-}
